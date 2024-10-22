2acaa3cdf989188eb4da4c41515fa32ad8f102b5
arnasbr <arnas.bradauskas@traveltime.com>
2024-10-22 17:07:29 +0300

    remove comment

--- example/codec.js
+++ example/codec.js
@@ -1,7 +1,7 @@
 import protobuf from 'k6/x/protobuf';
 
 const data = open("example.json");
-const protoFile = protobuf.load("example/v1/example.proto", "CountryList") // todo: Fix paths
+const protoFile = protobuf.load("example/v1/example.proto", "CountryList")
 
 export default function () {
     console.log(protoFile.decode(protoFile.encode(data)))
