14bb54d743d9ba1bcbd96241a7a4e58cc0b7dc6f
arnasbr <arnas.bradauskas@traveltime.com>
2024-10-22 13:35:33 +0300

    New directory structure, proto spread out over multiple files

--- country/codec.js
+++ country/codec.js
@@ -1,7 +1,7 @@
 import protobuf from 'k6/x/protobuf';
 
 const data = open("example.json");
-const protoFile = protobuf.load("examples/example.proto", "CountryList") // todo: Fix paths
+const protoFile = protobuf.load("country/v1/example.proto", "CountryList") // todo: Fix paths
 
 export default function () {
     console.log(protoFile.decode(protoFile.encode(data)))
